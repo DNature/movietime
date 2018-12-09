06067eda8026427e7b1dc4a26f3899a11989aaa0
Udeji Francis <francis.udejiofficial@gmail.com>
2018-12-09 23:02:55 +0100

    Finished setting booking routes and paystack

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/pages/Home";
 import SingleMovie from "./components/pages/SingleMovie";
+import Payment from "./components/pages/Payment";
+import Booking from "./components/pages/Booking";
 
 class App extends Component {
   render() {
@@ -14,6 +16,11 @@ class App extends Component {
             <Route
               path="/movie/:id/:title/:poster_path/:runtime/:release_date/:overview/view"
               component={SingleMovie}
+            />
+            <Route
+              exact
+              path="/movie/:title/:prefferedDate/:prefferedTime/:runtime/book"
+              component={Booking}
             />
           </Switch>
         </React.Fragment>
--- src/components/pages/SingleMovie.js
+++ src/components/pages/SingleMovie.js
@@ -1,20 +1,51 @@
 import React, { Component } from "react";
 import format from "date-fns/format";
 import axios from "axios";
-import VideoFrame from '../VideoFrame'
+import VideoFrame from "../VideoFrame";
+// import { Redirect, history } from "react-router-dom";
+import PaystackButton from "react-paystack";
 
 class SingleMovie extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: "",
       id: "",
       apiKey: `618b753712ed0a388d986c9e485d9a7c`,
       imageBaseURL: "https://image.tmdb.org/t/p/",
       imageSize: "w154/",
-      trailerLink: ""
+      trailerLink: "",
+      key: "pk_test_fa8cde05629cb0c2cb5e2e1daddd6d01a8b2bdf3", //PAYSTACK PUBLIC KEY
+      email: "francis.udejiofficial@gmail.com", // customer email
+      amount: 100000, //equals NGN100,
+      prefferedDate: "",
+      prefferedTime: "",
+      title: "",
+      poster_path: "",
+      runtime: "",
+      release_date: "",
+      overview: ""
     };
   }
+
+  callback = response => {
+    console.log(response); // card charged successfully, get reference here
+  };
+
+  close = () => {
+    console.log("Payment closed");
+  };
+
+  getReference = () => {
+    //you can put any unique reference implementation code here
+    let text = "";
+    let possible =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-.=";
+
+    for (let i = 0; i < 15; i++)
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+  };
 
   getTrailer = movieId => {
     /* Fetch single or multiple movie trailers via the TMDB API */
@@ -25,7 +56,7 @@ class SingleMovie extends Component {
     axios
       .get(url)
       .then(res => {
-        console.log(res.data);
+        //console.log(res.data);
         this.setState({
           trailerLink:
             "https://www.youtube.com/embed/" + res.data.results[0].key
@@ -36,6 +67,22 @@ class SingleMovie extends Component {
 
   componentDidMount() {
     this.getTrailer(this.props.match.params.id);
+    const {
+      //id,
+      title,
+      poster_path,
+      runtime,
+      release_date,
+      overview
+    } = this.props.match.params;
+
+    this.setState({
+      title,
+      poster_path,
+      runtime,
+      release_date,
+      overview
+    });
   }
 
   minuteToHour = timeInMins => {
@@ -56,11 +103,25 @@ class SingleMovie extends Component {
     return runtime;
   };
 
+  onInputChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  sendToBookingPage = e => {
+    e.preventDefault();
+    const { prefferedDate, prefferedTime, title, runtime } = this.state;
+    this.props.history.push(
+      `/movie/${title}/${prefferedDate}/${prefferedTime}/${runtime}/book`
+    );
+  };
+
   render() {
-    console.log(this.props);
+    //console.log(this.props);
     const { imageBaseURL, imageSize, trailerLink } = this.state;
     const {
-      id,
+      //id,
       title,
       poster_path,
       runtime,
@@ -72,7 +133,6 @@ class SingleMovie extends Component {
       <>
         <header>
           <div className="container">
-
             <nav
               className="navbar navbar-expand-lg navbar-dark"
               style={{ zIndex: "100", background: "transparent" }}
@@ -88,10 +148,10 @@ class SingleMovie extends Component {
               {trailerLink === null ? (
                 <p className="lead mx-auto text-center my-5 text-white">
                   Loading Trailer...
-                  </p>
+                </p>
               ) : (
-                  <VideoFrame src={trailerLink} />
-                )}
+                <VideoFrame src={trailerLink} />
+              )}
             </div>
 
             <div className="row text-white mt-5">
@@ -110,6 +170,7 @@ class SingleMovie extends Component {
                   className="lead text-white"
                   style={{ fontSize: "1rem", fontWeight: 500 }}
                 >
+                  {" "}
                   <span
                     style={{
                       color: "#ccc",
@@ -125,6 +186,7 @@ class SingleMovie extends Component {
                   className="lead text-white"
                   style={{ fontSize: "1rem", fontWeight: 500 }}
                 >
+                  {" "}
                   Release Date
                   <br />
                   <span
@@ -154,6 +216,70 @@ class SingleMovie extends Component {
                     {this.minuteToHour(runtime)}
                   </span>
                 </p>
+              </div>
+            </div>
+
+            <div className="row my-5">
+              <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 mx-auto">
+                <form className="form" onSubmit={this.sendToBookingPage}>
+                  <div className="form-group">
+                    <label
+                      htmlFor="exampleFormControlSelect1"
+                      style={{
+                        color: "#ccc",
+                        fontWeight: 400,
+                        fontSize: "1rem"
+                      }}
+                    >
+                      Choose a preffered date
+                    </label>
+                    <select
+                      className="form-control form-control-lg"
+                      id="exampleFormControlSelect1"
+                      name="prefferedDate"
+                      onChange={this.onInputChange}
+                      required
+                    >
+                      <option>Choose a preffered date</option>
+                      <option>Sat, 08 Dec</option>
+                      <option>Sat, 08 Dec</option>
+                      <option>Sun, 09 Dec</option>
+                      <option>Mon, 10 Dec</option>
+                      <option>Tue, 11 Dec</option>
+                      <option>Wed, 12 Dec</option>
+                    </select>
+                  </div>
+
+                  <div className="form-group">
+                    <label
+                      htmlFor="exampleFormControlSelect1"
+                      style={{
+                        color: "#ccc",
+                        fontWeight: 400,
+                        fontSize: "1rem"
+                      }}
+                    >
+                      Choose a preffered time
+                    </label>
+                    <select
+                      className="form-control form-control-lg"
+                      id="exampleFormControlSelect1"
+                      name="prefferedTime"
+                      onChange={this.onInputChange}
+                      required
+                    >
+                      <option>Choose a preffered time</option>
+                      <option>5:10 PM</option>
+                      <option>9:50 PM</option>
+                    </select>
+                  </div>
+                  <input
+                    type="submit"
+                    value="Continue"
+                    className="btn btn-danger btn-lg btn-block"
+                    onClick={this.sendToBookingPage}
+                  />
+                </form>
               </div>
             </div>
           </div>
